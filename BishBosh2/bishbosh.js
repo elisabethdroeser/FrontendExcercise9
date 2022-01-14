88c8a641d8d8060f0a086b4233c56ab1ab7ebb56
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-01-14 14:42:34 +0100

    Add query to button. for bishbosh

--- BishBosh2/bishbosh.js
+++ BishBosh2/bishbosh.js
@@ -3,19 +3,18 @@
 const number = document.querySelector('#num3');
 const bish = document.querySelector('#bish');
 const bosh = document.querySelector('#bosh');
-const button = document.querySelector('#btn');
+const submit = document.querySelector('#submit');
+
 
 function myFunction() {
   
     for (let i = 1; i <= number.value; i++) {
-        console.log(number.value);
-
-        if (i % bish.value === 0 && i % bosh.value === 0) {
-        
+        //console.log(number.value);
+        if (i % bish.value === 0 && i % bosh.value === 0) {  
             console.log('Bish-Bosh')
         }
         else if ( i % bish.value === 0) {
-            let text = document.innerHTML(bish.value);
+            //let text = document.innerHTML(bish.value);
             console.log('Bish')
         }
         else if (i % bosh.value === 0) {
@@ -26,5 +25,7 @@ function myFunction() {
         }
     }
 }
+
+document.querySelector('#submit').addEventListener('click', myFunction);
 
 
